1a12ca3720c051b47dbfa175ff860c47d1094f05
Ramki Thurimella <rthurimella@yahoo.com>
2023-07-12 14:50:26 -0600

    Added more functionality to Main

--- src/main/java/org/thurimella/Main.java
+++ src/main/java/org/thurimella/Main.java
@@ -8,36 +8,58 @@ import org.thurimella.util.BloomFilter;
 import java.util.Random;
 
 public class Main {
-    private static final int MAX_KEY = 999999999;
+    private static final int MAX_KEY = 9999999;
     private static final int NUM_ELEMENTS = 1000000;
-    private static final int NUM_SEARCHES = 100000;
+    private static final int NUM_SEARCHES = 500000;
     private static final int FILTER_SIZE_BITS = 12000000;
 
     public static void main(String[] args) {
         // Create BST and Bloom Filter
         BST bst = new BST();
+        BST bstWithoutFilter = new BST();
+
         BloomFilter filter = new BloomFilter(FILTER_SIZE_BITS);
 
-        // Step 1: Create set S and store it in BST T
-        createSetAndStoreInBST(bst, filter);
+        // Step 1: Create a set S and store it in BST T
+        createSetAndStoreInBST(bst, bstWithoutFilter, filter);
 
         // Step 2: Perform random searches and print search time
         performRandomSearches(bst, filter);
     }
 
-    private static void createSetAndStoreInBST(BST bst, BloomFilter filter) {
+    private static void createSetAndStoreInBST(BST bst, BST bstWithoutFilter, BloomFilter filter) {
         Random random = new Random();
+        long startTime, endTime;
+        double elapsedTime;
+
+        startTime = System.nanoTime();
+        for (int i = 0; i < NUM_ELEMENTS; i++) {
+            int randomInteger = random.nextInt(MAX_KEY + 1);
+            bstWithoutFilter.insert(randomInteger);
+        }
+        endTime = System.nanoTime();
+        elapsedTime = (endTime - startTime) / 1_000_000.0;
+        System.out.println();
+        System.out.println("Set creation *without* Bloom Filter took: " + elapsedTime + " milliseconds");
+
+        startTime = System.nanoTime();
         for (int i = 0; i < NUM_ELEMENTS; i++) {
             int randomInteger = random.nextInt(MAX_KEY + 1);
             bst.insert(randomInteger);
             filter.insertElement(randomInteger);
         }
+        endTime = System.nanoTime();
+        elapsedTime = (endTime - startTime) / 1_000_000.0;
+
+        System.out.println("Set creation *with* Bloom Filter took: " + elapsedTime + " milliseconds");
+        System.out.println();
     }
 
     private static void performRandomSearches(BST bst, BloomFilter filter) {
         Random random = new Random();
         long startTime, endTime;
         double elapsedTime;
+        int noOfFalsePositives = 0;
 
         // Search without using Bloom Filter
         startTime = System.nanoTime();
@@ -47,19 +69,23 @@ public class Main {
         }
         endTime = System.nanoTime();
         elapsedTime = (endTime - startTime) / 1_000_000.0;
-        System.out.println("Search without Bloom Filter took: " + elapsedTime + " milliseconds");
+        System.out.println("Search *without* Bloom Filter took: " + elapsedTime + " milliseconds");
 
         // Search using Bloom Filter
         startTime = System.nanoTime();
         for (int i = 0; i < NUM_SEARCHES; i++) {
             int searchValue = random.nextInt(MAX_KEY + 1);
             if (filter.presentElement(searchValue)) {
-                bst.found(searchValue);
+                if (!bst.found(searchValue)) noOfFalsePositives++;
             }
         }
         endTime = System.nanoTime();
         elapsedTime = (endTime - startTime) / 1_000_000.0;
-        System.out.println("Search with Bloom Filter took: " + elapsedTime + " milliseconds");
+        System.out.println("Search *with* Bloom Filter took: " + elapsedTime + " milliseconds");
+        System.out.println();
+
+        System.out.println("Number of False Positives: " + noOfFalsePositives + " or " + (noOfFalsePositives * 100/NUM_SEARCHES) + "%");
+
     }
 }
 
